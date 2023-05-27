94b5b5b798e7a367c08072247785ecaa922b656f
EugeneyCh <eugeney@ua.fm>
2023-05-27 11:34:46 +0300

    Profile_creating

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
+import data from './Profile/data.json';
+import friends from './Profile/friends.json';
+import transactions from './Profile/transactions.json';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      {/* React homework-01 */}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
