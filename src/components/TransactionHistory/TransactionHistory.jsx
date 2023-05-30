0574bf708dc22b7c145ece89bd81107141b07eae
EugeneyCh <eugeney@ua.fm>
2023-05-30 22:25:02 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -27,13 +27,13 @@ function TransactionHistory(items) {
           <td>USD</td>
         </tr>
 
-        {items.map(item => (
+        {/* {items.map(item => (
           <tr>
             <td>{item.type}</td>
             <td>{item.amount}</td>
             <td>{item.currency}</td>
           </tr>
-        ))}
+        ))} */}
       </tbody>
     </table>
   );
