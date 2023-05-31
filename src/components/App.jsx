55dc2d6fa6e363c6e8b8d35bb683d1b158d99036
EugeneyCh <eugeney@ua.fm>
2023-05-31 23:23:31 +0300

    Editing_way_to_json_files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
-import user from './Profile/user.json';
-import data from './Profile/data.json';
-import friends from './Profile/friends.json';
-import transactions from './Profile/transactions.json';
+import user from './user.json';
+import data from './data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
 
 export const App = () => {
   return (
